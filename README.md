4c98e62ce5201988715561a7ca692c662318603c
Duncan McGreggor <oubiwann@gmail.com>
2023-06-14 00:53:25 -0500

    Updated categroies.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # etf
+
 Racket support for Erlang's [External Term Format](https://www.erlang.org/doc/apps/erts/erl_ext_dist.html), allowing Racket programs to communicate directly with Erlang nodes.
+
+[Package](https://pkgs.racket-lang.org/).
--- etf/info.rkt
+++ etf/info.rkt
@@ -1,9 +1,11 @@
 #lang setup/infotab
 (define name "etf")
+(define version "0.0.0")
 (define blurb
   (list
    `(p "Racket support for Erlang's External Term Format, allowing Racket programs to communicate directly with Erlang nodes. ")))
-(define categories '(net))
+(define license '(Apache-2.0))
+(define categories '(io net))
 (define can-be-loaded-with 'all)
 (define homepage "https://github.com/racaid/etf")
 (define primary-file "main.rkt")
